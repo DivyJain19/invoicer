6d20872d2ea3e2d067805934f48cc08a77f06c68
Dexter1145 <divy.jain.19@gmail.com>
2024-03-04 23:28:56 +0530

    first commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+router.route('/signup').post(userController.registerUser);
+router.route('/login').post(userController.authUser);
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoutes = require('./routes/userRoutes');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
+const cors = require('cors');
+dotenv.config();
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB Connection Successful!'))
+  .catch((err) => {
+    console.log('Error 💥', err);
+  });
+app.use(cors());
+app.use(express.json());
+app.use('/api/users', userRoutes);
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`App Running On Port ${port}`);
+});
