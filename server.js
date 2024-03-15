69610a51e44e4ce9654d3324dfb6f92c0dd9404c
Dexter1145 <divy.jain.19@gmail.com>
2024-03-16 04:25:24 +0530

    app use in the end

--- server.js
+++ server.js
@@ -22,9 +22,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
-app.use("/" , (req,res) => {
-  res.send("Server is Running")
-})
+
 app.use('/api/users', userRoutes);
 app.use('/api/company', companyRoutes);
 app.use('/api/product', productRoutes);
@@ -32,6 +30,9 @@ app.use('/api/entry', entryRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
+app.use("/" , (req,res) => {
+  res.send("Server is Running")
+})
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`App Running On Port ${port}`);
